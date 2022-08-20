1b569343f563ac608dd00198091825ebdfed5e65
Ml06py <liaghimohamad69@gmail.com>
2022-08-20 18:10:18 +0430

    Product model added

--- apps/vendor/admin.py
+++ apps/vendor/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from vendor.models import Category, Country
+from vendor.models import Category, Country, Product
 
 admin.site.register(Category)
 admin.site.register(Country)
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ("title", "seller", "price")
--- apps/vendor/models.py
+++ apps/vendor/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from authentication.models import Account
 
 class Category(models.Model):
 
@@ -21,3 +21,24 @@ class Country(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=120, blank=True, null=True)
+    description = models.TextField()
+
+    picture = models.ImageField(upload_to="products/%Y-%m-%d", blank=True, null=True)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", blank=True, null=True)
+    shipping_from = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="shipping_from", blank=True, null=True)
+    shipping_to = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True, related_name="shipping_to")
+    seller = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True, null=True, related_name="products")
+
+    price = models.IntegerField(default=0)
+    is_available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
