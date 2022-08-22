52229bf91732c184ab7a9b281510919ba75bf6b1
Ml06py <liaghimohamad69@gmail.com>
2022-08-22 14:19:19 +0430

    Message model added

--- apps/message/admin.py
+++ apps/message/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from message.models import Chat
+from message.models import Chat, Message
 
 
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
     list_display = ("creator", "member", "code")
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("chat", "sender", "is_seen")
--- apps/message/models.py
+++ apps/message/models.py
@@ -11,3 +11,20 @@ class Chat(models.Model):
 
     def __str__(self):
         return f"{self.creator} | {self.member}"
+
+
+class Message(models.Model):
+    '''Message text model'''
+
+    text = models.TextField()
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name="chats", blank=True,null=True)
+    sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="messages", blank=True,null=True)
+
+    is_edited = models.BooleanField(default=False)
+    is_seen = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.pk} | {self.sender.username}"
+
+
