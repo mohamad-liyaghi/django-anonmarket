53da943f21110e60a43e126f4a60bac47f0574c3
Ml06py <liaghimohamad69@gmail.com>
2022-08-22 15:14:46 +0430

    Get chat function added

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "message"
 
 urlpatterns = [
-    path("", views.ChatList.as_view(), name="chat-list")
+    path("", views.ChatList.as_view(), name="chat-list"),
+    path("get-chat/<int:id>/<str:token>/", views.GetChat.as_view(), name="get-chat")
 ]
--- apps/message/views.py
+++ apps/message/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, get_object_or_404
+from django.db.models import Q
+
+import random
+
+from authentication.models import Account
+from message.models import Chat
 
 
 class ChatList(LoginRequiredMixin, TemplateView):
@@ -15,3 +22,27 @@ class ChatList(LoginRequiredMixin, TemplateView):
 
 
         return context
+
+
+
+class GetChat(LoginRequiredMixin, View):
+    '''Create or Connect a user to a chat'''
+
+    def get(self, request, id, token):
+        # get the user that we want to connect
+        user = get_object_or_404(Account, id=id, token=token)
+
+        # check if this chat has created before
+        existing_chat = Chat.objects.filter(Q(creator= user) & Q(member= self.request.user)
+                                             | Q(creator= self.request.user) & Q(member= user))
+
+        # if this chat is new, we create a new chat
+        if not existing_chat.exists():
+            existing_chat = Chat.objects.create(creator= self.request.user, member= user,
+                                code= random.randint(-2147483648, 2147483647))
+
+        # then we redirect to the chat detail page
+        return redirect("message:chat-detail", existing_chat.pk, existing_chat.code)
+
+
+
