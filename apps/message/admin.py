e48df3ad50b561ac954ccc36fcbf5d145ca8501e
Ml06py <liaghimohamad69@gmail.com>
2022-08-22 14:03:05 +0430

    Chat model added

--- apps/message/admin.py
+++ apps/message/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from message.models import Chat
 
-# Register your models here.
+
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("creator", "member", "code")
--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from authentication.models import Account
 
-# Create your models here.
+
+class Chat(models.Model):
+    '''chat model for users in order to send messages'''
+
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="chat_creator" ,blank=True, null=True)
+    member = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="chat_member" ,blank=True, null=True)
+    code = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.creator} | {self.member}"
