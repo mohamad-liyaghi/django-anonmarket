0fa964bbc2a0c4481a51f5974237dd07d81f16c7
Ml06py <liaghimohamad69@gmail.com>
2022-08-26 15:10:53 +0430

    Article model added

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.models import Article
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "published")
+    prepopulated_fields = {"slug" : ("title",)}
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from vendor.models import Product
+from authentication.models import Account
+
+
+class Article(models.Model):
+    '''The article model'''
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField()
+
+    body = models.TextField()
+
+    author = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="articles", blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="articles", blank=True, null=True)
+
+    # this field is just for vip blogs
+    price = models.IntegerField(default=0)
+    allowed_members = models.ManyToManyField(Account, blank=True, related_name="allowed_members")
+
+    published = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
