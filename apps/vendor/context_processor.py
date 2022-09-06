d17ee8f50a48a33dc228c1e3cb5da455b8468eef
Ml06py <liaghimohamad69@gmail.com>
2022-09-06 11:38:38 +0430

    cart context processor added

---
+++ apps/vendor/context_processor.py
@@ -0,0 +1,7 @@
+def cart_items_counter(request):
+    """Return Number of products in the cart"""
+
+    if request.user.is_authenticated:
+        return {"cart_count" : request.user.orders.count}
+
+    return None
--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.vendor.context_processor.cart_items_counter',
             ],
         },
     },
