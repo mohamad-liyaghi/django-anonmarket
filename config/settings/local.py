80129040d4fbfd1c43215ce4eacdb2d4b757c003
Ml06py <liaghimohamad69@gmail.com>
2022-09-07 12:02:40 +0430

    Multiple settings added

---
+++ config/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
