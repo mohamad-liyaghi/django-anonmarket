f88f35821e56ff4440515c2f289f8ade75b59d2f
Ml06py <liaghimohamad69@gmail.com>
2022-12-18 12:32:12 +0330

    Vote model added

--- apps/vote/admin.py
+++ apps/vote/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from vote.models import Vote
 
-# Register your models here.
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ["user", "vote", "content_object"]
--- apps/vote/models.py
+++ apps/vote/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Vote(models.Model):
+    '''A vote model that can be used for products, users, etc.'''
+
+    class Choice(models.TextChoices):
+        like = ("l", "Like")
+        dislike = ("d", "Dislike")
+
+    vote = models.CharField(max_length=1, choices=Choice.choices, default=Choice.like)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self) -> str:
+        return self.vote
