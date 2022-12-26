2536b6281cf4c4e1558c5a9d7135e211de7f3f12
Ml06py <liaghimohamad69@gmail.com>
2022-12-26 14:32:27 +0330

    Comment model added

--- apps/comment/admin.py
+++ apps/comment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from comment.models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["user", "content_object", "date"]
--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,3 +1,38 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, 
+                            related_name="comments")
+
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True,
+                                related_name="children")   
+
+    date = models.DateTimeField(auto_now_add=True)                                
+    content = models.TextField()
+
+    edited = models.BooleanField(default=False)
+    pinned = models.BooleanField(default=False)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+    def __str__(self) -> str:
+        if not self.parent:
+            return f"Comment by {self.user}: {self.content[:20]}"
+
+        return f"Reply by {self.user}: {self.content[:20]}"
+
+    @property
+    def is_edited(self):
+        return self.edited
+    
+
+
+
