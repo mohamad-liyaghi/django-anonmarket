cc8f9c0ff4976bd8953dac56bb15b65ca7f8b4b6
Ml06py <liaghimohamad69@gmail.com>
2022-12-28 15:59:30 +0330

    Comment list added

--- apps/comment/templatetags/comment_tags.py
+++ apps/comment/templatetags/comment_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 def get_object_comments(obj):
     #TODO add rate to orders
     if obj.comment:
-        return obj.comment.all().order_by("-pinned", "-date")
+        return obj.comment.filter(parent=None).order_by("-pinned", "-date")
 
     return None
 
