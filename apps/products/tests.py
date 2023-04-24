b3d3a73ca464053260d1b5322ae748c6071eb169
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-04-24 16:25:47 +0330

    Product tests added

--- apps/products/tests.py
+++ apps/products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from accounts.models import Account
+from products.models import Product, Category
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self) -> None:
+        self.user = Account.objects.create(username='test username', password='1234EErr')
+        self.product = Product.objects.create(title='title', provider=self.user)
+        self.category = Category.objects.create(title='food')
+    
+    def test_slug_generator_for_product(self):
+        self.assertTrue(self.product.slug)
+
+    def test_slug_generator_for_category(self):
+            self.assertTrue(self.product.slug)
+
+    def test_category_name_lower(self):
+         self.assertEqual(self.category.title, self.category.title.lower())
