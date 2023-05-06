18f835b51e1ed559a42f32d0d7ea6255f9582c1f
Mohamad Liyaghi <liaghimohamad69@gmail.com>
2023-05-06 16:04:28 +0330

    Vote model updated

--- apps/votes/admin.py
+++ apps/votes/admin.py
@@ -3,4 +3,4 @@ from votes.models import Vote
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ["user", "vote", "content_object"]
+    list_display = ["user", "choice", "content_object"]
--- apps/votes/models.py
+++ apps/votes/models.py
@@ -5,15 +5,16 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 from votes.managers import VoteManager
 
+
 class Vote(models.Model):
-    '''A vote model that can be used for products, users, etc.'''
+    '''Generic vote model'''
 
     class Choice(models.TextChoices):
-        like = ("l", "Like")
-        dislike = ("d", "Dislike")
+        UPVOTE = ("u", "UpVote")
+        DOWNVOTE = ("d", "DownVote")
 
-    vote = models.CharField(max_length=1, choices=Choice.choices, default=Choice.like)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=1, choices=Choice.choices, default=Choice.UPVOTE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='votes')
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
@@ -22,4 +23,7 @@ class Vote(models.Model):
     objects = VoteManager()
 
     def __str__(self) -> str:
-        return self.vote
+        return self.choice
+    
+    class Meta:
+        unique_together = ['user', 'content_type', 'object_id']
